c35e59fd6d686b888935589b9ffa50bdc51fe9cf
gabriel cruz <gabrielcruzvazsantos@gmail.com>
2024-09-18 11:08:35 -0300

    fix: limit map gigant hitbox

--- helpers/inputs.s
+++ helpers/inputs.s
@@ -24,34 +24,43 @@ INPUT: 	li t1,0xFF200000
 
 INPUT.W: la t0, SAMUS_POSITION
 		lw t1, 4(t0)
+		li t2, 40
+		ble t1, t2, INPUT_RET
 		addi t1, t1, -32
 		sw t1, 4(t0)
 		
-		ret
+		j INPUT_RET
 
 INPUT.S: la t0, SAMUS_POSITION
 		lw t1, 4(t0)
+		li t2, 170
+		bge t1, t2, INPUT_RET
 		addi t1, t1, 16
 		sw t1, 4(t0)
 		
-		ret
+		j INPUT_RET
 
 INPUT.A: la t0, MAP_POSITION
 		lw t1, 0(t0)
+		li t2, 120
+		ble t1, t2, INPUT_RET
 		addi t1, t1, -20
 		sw t1, 0(t0)
 		
-		ret
+		j INPUT_RET
 
 INPUT.D: la t0, MAP_POSITION
 		lw t1, 0(t0)
+		li t2, 1640
+		bge t1, t2, INPUT_RET
 		addi t1, t1, 20
 		sw t1, 0(t0)
 		
-		ret
+		j INPUT_RET
 
 
 END_INPUT:	la t0, SAMUS_JUMP
 		sw zero, 0(t0)		# zera moveX, moveY, jump e roll (cada um eh um byte, usamos word para zerar os 4 ja que est�o um em seguida do outro na memoria)
-		ret			# 
+		j INPUT_RET			
 				
+INPUT_RET: ret
